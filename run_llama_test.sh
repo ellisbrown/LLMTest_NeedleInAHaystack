3dbfe1f0d93cf1380edf12f97f3c5233672ab13f
Ellis Brown <ellis.l.brown.ii@gmail.com>
2024-07-02 09:36:28 -0700

    add local llama

---
+++ run_llama_test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export NIAH_EVALUATOR_API_KEY=$OPENAI_API_KEY
+MODEL=meta-llama/Llama-2-7b-chat-hf
+
+source venv/bin/activate
+
+python -m needlehaystack.run \
+    --provider local \
+    --model_name $MODEL \
+    --document_depth_percents "[50]" \
+    --context_lengths "[2000]"
