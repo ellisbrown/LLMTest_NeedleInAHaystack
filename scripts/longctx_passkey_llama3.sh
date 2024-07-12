177ba97609f35b6a2e80506e6c5df26505a7137e
Ellis Brown <ellis.l.brown.ii@gmail.com>
2024-07-11 17:30:41 -0700

    long ctx passkey test

--- scripts/longctx_passkey_llama3.sh
+++ scripts/longctx_passkey_llama3.sh
@@ -3,12 +3,11 @@
 export NIAH_EVALUATOR_API_KEY=$OPENAI_API_KEY
 MODEL=meta-llama/Meta-Llama-3-8B-Instruct
 
-MIN_CTX_LEN=128
-MAX_CTX_LEN=8192
-N_CTX=35
-N_DEPTH=35
-
-OUTPUT_DIR=./results/linear_${N_CTX}x${N_DEPTH}_llama_3-8b-instruct
+MIN_CTX_LEN=4096
+MAX_CTX_LEN=16384
+N_CTX=48
+N_DEPTH=5
+OUTPUT_DIR=./results/long_passkey_ctx_${MIN_CTX_LEN}-${MAX_CTX_LEN}_${N_CTX}x${N_DEPTH}_llama_3-8b-instruct
 
 source venv/bin/activate
 python -m needlehaystack.run \
@@ -19,6 +18,10 @@ python -m needlehaystack.run \
     --context_lengths_num_intervals $N_CTX \
     --document_depth_percent_intervals $N_DEPTH \
     --context_lengths_max $MAX_CTX_LEN \
+    --haystack_dir "PassKey" \
+    --needle "The pass key is 9054. Remember it. 9054 is the pass key." \
+    --retrieval_question "What is the pass key? The pass key is"
+
 
 # generate plot
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
